e41ec5ca2cfd1a914ab0e16a7bad09f9b155bb6e
josoowoon <jsw.joongwon@daum.net>
2019-11-28 10:28:16 +0900

    First commit
    - It will be make by JavaScript.

---
+++ FirstCocosJS/assets/Script/HelloWorld.js
@@ -0,0 +1,27 @@
+cc.Class(
+{
+    extends: cc.Component,
+
+    properties:
+    {
+        label:
+        {
+            default: null,
+            type: cc.Label
+        },
+        
+        // defaults, set visually when attaching this script to the Canvas
+        text: 'Hi, there'
+    },
+
+    // use this for initialization
+    onLoad: function () 
+    {
+        this.label.string = this.text;
+    },
+
+    // called every frame
+    update: function (dt)
+    {
+    },
+});
