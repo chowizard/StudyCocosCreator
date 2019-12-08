3d1d929cab6717092f74ea6b9ac7981835ee3355
chowizard <chowizard@hotmail.com>
2019-12-09 01:29:31 +0900

    [Fixed] Fixed a bug that always running specific scene when doing play test through Cocos Creator editor.

--- FirstCocosJS/assets/Script/GameManager.ts
+++ FirstCocosJS/assets/Script/GameManager.ts
@@ -1,7 +1,16 @@
+enum eCharacterClass
+{
+    Unknown = 0,
+    Worrior = 1,
+    Archer = 2,
+    Wizard = 3,
+}
+
+
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component
+export default class GameManager extends cc.Component
 {
     @property(cc.Label)
     label: cc.Label = null;
@@ -26,10 +35,13 @@ export default class NewClass extends cc.Component
     {
         let age : number = 37;
         let testText : string = 
-        `Hello,
-        My name is Jo Soowoon.
-        I'm ${age} years old.`;
+`Hello,
+My name is Jo Soowoon.
+I'm ${age} years old.`;
         console.log(testText);
+
+        let characterClass : eCharacterClass = eCharacterClass.Archer;
+        console.log("GameManager.start() : CharacterClass = " + eCharacterClass[characterClass]);
     }
 
     /**
