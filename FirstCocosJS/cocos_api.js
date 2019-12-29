49bb7b7d6bdc10ec6fe7ac4f8c48791171147b25
chowizard <chowizard@hotmail.com>
2019-12-29 23:15:44 +0900

    [Feature] Added cocos_api.js file for to run JavaScript intellisense with Cocos Creator project.
    - https://discuss.cocos2d-x.org/t/js-auto-code-completion-not-working/45673

---
+++ FirstCocosJS/cocos_api.js
@@ -0,0 +1,9 @@
+/**
+ * DESC - 2019-12-29 조수운
+ * : 이유는 모르겠는데, v2.2.1 기준으로, JavaScript로 제작하는 경우에, Cocos 엔진 API에 대한 인텔리센스를 이용할 수가 없다.
+ *  이 때문에 이와 같은 파일을 수동으로 만들어줘야 한다.
+ * 
+ * 참조 : https://discuss.cocos2d-x.org/t/js-auto-code-completion-not-working/45673/22
+ */
+
+///<reference path="creator.d.ts" />
